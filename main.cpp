3faa2088fff56ee740283a50ca7d216e42724984
RCEcom <87273590+RCEcom@users.noreply.github.com>
2022-01-27 21:02:07 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
+//Author : RCEcom
 #include <iostream>
-#include "CArr.h"
-
+#include <string.h>
+#pragma warning(disable:4996)
 namespace mystd
 {
 	void myendl()
 	{
-		wprintf(L"\n");
+		printf("\n");
 	}
 
 	class CMyOStream
 	{
+	public:
+		void precisions(float _idata, int c);
+		void precisions(double _idata, int c);
+		void precisions(long double _idata, int c);
 	public:
 		CMyOStream& operator << (int _idata)
 		{
 			printf("%d", _idata);
 			return *this;
 		}
-		CMyOStream& operator << (const wchar_t* _idata)
+		CMyOStream& operator << (const char* _idata)
 		{
-			wprintf(L"%s", _idata);
+			printf("%s", _idata);
+			return *this;
+		}
+		CMyOStream& operator << (float _idata)
+		{
+			printf("%f", _idata);
+			return *this;
+		}
+		CMyOStream& operator << (double _idata)
+		{
+			printf("%f", _idata);
+			return *this;
+		}
+		CMyOStream& operator << (long double _idata)
+		{
+			printf("%Lf", _idata);
 			return *this;
 		}
 		CMyOStream& operator << (void(*pfuc)(void))
@@ -31,20 +51,99 @@ namespace mystd
 	class CMyOstream_input
 	{
 	public:
+		void get(char buf[], unsigned int _size);
+	public:
 		CMyOstream_input& operator >> (int& _idata)
 		{
 			scanf_s("%d", &_idata);
 			return *this;
 		}
+		CMyOstream_input& operator >> (char* _idata)
+		{
+			scanf_s("%s", _idata);
+			return *this;
+		}
+		CMyOstream_input& operator >> (float& _idata)
+		{
+			scanf_s("%f", &_idata);
+			return *this;
+		}
+		CMyOstream_input& operator >> (double& _idata)
+		{
+			scanf_s("%f", &_idata);
+			return *this;
+		}
+		CMyOstream_input& operator >> (long double& _idata)
+		{
+			scanf_s("%Lf", &_idata);
+			return *this;
+		}
+
 	};
 	CMyOstream_input mycin;
 	CMyOStream mycout;
+	void CMyOstream_input::get(char *buf, unsigned int _size)
+	{
+		scanf_s("%s", buf, _size);
+	}
+	void CMyOStream::precisions(float _idata, int c)
+	{
+		if (c == 2)
+		{
+			printf("%.2f", _idata);
+		}
+		if (c == 3)
+		{
+			printf("%.3f", _idata);
+		}
+		if (c == 4)
+		{
+			printf("%.4f", _idata);
+		}
+	}
+	void CMyOStream::precisions(double _idata, int c)
+	{
+		if (c == 2)
+		{
+			printf("%.2f", _idata);
+		}
+		if (c == 3)
+		{
+			printf("%.3f", _idata);
+		}
+		if (c == 4)
+		{
+			printf("%.4f", _idata);
+		}
+	}
+	void CMyOStream::precisions(long double _idata, int c)
+	{
+		if (c == 2)
+		{
+			printf("%.2f", _idata);
+		}
+		if (c == 3)
+		{
+			printf("%.3f", _idata);
+		}
+		if (c == 4)
+		{
+			printf("%.4f", _idata);
+		}
+	}
 }
-int main(void)
+
+using mystd::mycin;
+using mystd::mycout;
+using mystd::myendl;
+int main()
 {
-	//직접 구현한 std::cout, std::endl, std::cin
-	int a = 0;
-	mystd::mycin >> a;
-	mystd::mycout << L"a value? : " << a << L" be." << mystd::myendl;
+	char* buf = new char[52]; //char buf[50]; ok :)
+	mycin.get(buf, 30);
+	mycout << buf;
+	double f_v = 123.45612345f;
+	mycout << f_v << myendl;
+	mycout.precisions(f_v, 2);
+	mycout << "" << myendl;
 	return 0;
-} 
+}
